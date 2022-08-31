068764584eccdffca30baa7309febde06faef3b4
Gabriel José <gabriel.josemelo@hotmail.com>
2022-08-31 11:34:42 -0300

    add read input function

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,49 @@
-use std::io::{self, Write};
+use std::{io::{self, Write}, process};
 
 fn main() {
+  clear();
+  welcome();
+  io::stdout().flush().expect("Unexpected error on read line");
+
+  let command = read_input();
+
+  println!("Input: [{}]", command);
+
+  if command.eq("signin") {
     clear();
-    welcome();
-    io::stdout().flush().expect("Unexpected error on read line");
-
-    let mut name = String::new();
-
-    io::stdin()
-        .read_line(&mut name)
-        .expect("Failed to read line.")
-    ;
-
-    println!("Input: [{}]", name);
-
-    if name.trim().eq("signin") {
-        clear();
-        println!("Sign in!!!")
-    }
+    println!("Sign in!!!");
+  }
 }
 
 fn welcome() {
-    print_multi_lines([
-        "Welcome to the store",
-        "\tYou'r not current loged",
-        "\nCommands:",
-        "signin - Create an account",
-        "login - Log in an existing account"
-    ].to_vec());
+  print_multi_lines([
+    "Welcome to the store",
+    "\tYou'r not current loged",
+    "\nCommands:",
+    "signin - Create an account",
+    "login - Log in an existing account"
+  ].to_vec());
 
-    print!("> ");
+  print!("> ");
 }
 
 fn print_multi_lines(texts: Vec<&str>) {
-    for text in texts {
-        println!("{}", text);
-    }
+  for text in texts {
+    println!("{}", text);
+  }
 }
 
 fn clear() {
-    std::process::Command::new("clear").status().unwrap();
+  process::Command::new("clear").status().unwrap();
+}
+
+fn read_input() -> String {
+  let mut data = String::new();
+
+  io::stdin()
+    .read_line(&mut data)
+    .expect("Failed to read line.")
+  ;
+
+  return String::from(data.trim());
 }
