0d9c7a55aefa2d7078e6d46378113d3f5f53df68
Gabriel José <gabriel.josemelo@hotmail.com>
2022-08-31 15:24:10 -0300

    add sign in section

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,63 @@
-use std::{io::{self, Write}, process};
+use std::{io::{self, Write, Read}, process, thread, time};
 
 fn main() {
+  home();
+}
+
+fn home() {
   clear();
   welcome();
-  io::stdout().flush().expect("Unexpected error on read line");
+  flush_output();
 
-  let command = read_input();
+  let command = read_input(None);
 
-  println!("Input: [{}]", command);
-
-  if command.eq("signin") {
-    clear();
-    println!("Sign in!!!");
+  match command.as_ref() {
+    "signin" => sign_in(),
+    "q" => {
+      println!("\nExiting...")
+    },
+    _ => {
+      println!("\x1b[93m\n** Command not found **\n\x1b[0m");
+      thread::sleep(time::Duration::from_millis(500));
+      pause();
+      home();
+    }
   }
+}
+
+fn sign_in() {
+  clear();
+  println!("# Sign In\n\n");
+  let name = read_input(Some("Enter your name: "));
+  let password = read_input(Some("Enter your password: "));
+
+  println!("Name: {}, Password: {}", name, password);
+
+  home();
 }
 
 fn welcome() {
   print_multi_lines([
+    "# Home\n",
     "Welcome to the store",
-    "\tYou'r not current loged",
+    "\n\tYou're not current loged",
     "\nCommands:",
-    "signin - Create an account",
-    "login - Log in an existing account"
+    " signin - Create an account",
+    " login - Log in an existing account",
+    " q - Exit application"
   ].to_vec());
 
-  print!("> ");
+  print!("\n> ");
+}
+
+fn pause() {
+  let mut stdin = io::stdin();
+  let mut stdout = io::stdout();
+
+  write!(stdout, "Press ENTER to continue...").unwrap();
+  stdout.flush().unwrap();
+
+  let _ = stdin.read(&mut [0u8]).unwrap();
 }
 
 fn print_multi_lines(texts: Vec<&str>) {
@@ -37,7 +70,19 @@ fn clear() {
   process::Command::new("clear").status().unwrap();
 }
 
-fn read_input() -> String {
+fn flush_output() {
+  io::stdout().flush().expect("Unexpected error on read line");
+}
+
+fn read_input(text_before: Option<&str>) -> String {
+  match text_before {
+    Some(value) => {
+      print!("{}", value);
+      flush_output();
+    },
+    None => ()
+  }
+
   let mut data = String::new();
 
   io::stdin()
