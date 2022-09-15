b4b3681c48c6eeadd35bd8ac44c7629549e4221e
Gabriel José <gabriel.josemelo@hotmail.com>
2022-09-14 21:54:42 -0300

    finish login

--- src/sections/login.rs
+++ src/sections/login.rs
@@ -1,17 +1,24 @@
 use std::{fs::File, io::Read};
 
 use colored::Colorize;
-use serde_json::Value;
+use serde::{Deserialize, Serialize};
 
 use crate::{state::Account, terminal::in_out::{read_input, pause, clear}, sections::home, file::append_to_file};
 
 use super::components::header;
 
+#[derive(Serialize, Deserialize)]
+struct CreatedAccount {
+  id: String,
+  name: String,
+  password: String
+}
+
 pub fn login(account: Option<Account>) {
   clear();
   header("Login", &account);
 
-  let name = read_input(Some("\nEnter your name: "));
+  let name = read_input(Some("\nName: "));
 
   if name.eq("q") {
     println!("\nCancelling and returning to home...\n");
@@ -20,7 +27,7 @@ pub fn login(account: Option<Account>) {
     return;
   }
 
-  let password = read_input(Some("Enter your password: "));
+  let password = read_input(Some("Password: "));
 
   if password.eq("q") {
     println!("\nCancelling and returning to home...\n");
@@ -42,19 +49,29 @@ pub fn login(account: Option<Account>) {
   ;
 
   if json_data.is_empty() {
-    append_to_file("./src/data.json", "[]");
-    println!("\n{}\n", "** User not found **".yellow());
-    pause();
-    home(account)
+    return on_user_not_found();
   }
 
-  let users_list = match serde_json::from_str::<Value>(&json_data) {
-    Ok(data) => data,
-    Err(_) => {
-      println!("\nUnable to read file data");
-      Value::Array(vec![])
-    }
-  };
+  let users_list: Vec<CreatedAccount> = serde_json::from_str(&json_data)
+    .expect("JSON parse error")
+  ;
 
-  println!("Position one: {}", users_list[0]);
+  let user = users_list.into_iter().find(
+    |user| name.eq(&user.name) && password.eq(&user.password)
+  );
+
+  match user {
+    Some(user) => home(Some(Account {
+      id: user.id,
+      name: user.name
+    })),
+    None => on_user_not_found()
+  }
+}
+
+fn on_user_not_found() {
+  append_to_file("./src/data.json", "[]");
+  println!("\n{}\n", "** User not found **".yellow());
+  pause();
+  home(None);
 }
